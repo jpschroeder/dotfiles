3d5458bdde17a3bbfb7c16751c58f89956d5bbc3
John Schroeder <john@schroederspace.com>
2024-12-23 16:15:47 -0500

    start kitty on filetype

--- nvim/.config/nvim/lua/plugins/terminal.lua
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -1,6 +1,9 @@
 return {
   -- syntax highlighting for kitty config file: set ft=kitty
-  { 'fladson/vim-kitty' },
+  {
+    'fladson/vim-kitty',
+    ft = 'kitty',
+  },
 
   -- kitty integration
   { 'knubie/vim-kitty-navigator' },
