3ee69219bd789e6698c6e296af7d4b426c63a578
John Schroeder <john@schroederspace.com>
2025-06-26 17:34:32 -0400

    ui update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -14,5 +14,6 @@ return {
     vim.cmd.colorscheme 'onedark'
     vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'NONE' })
   end,
 }
